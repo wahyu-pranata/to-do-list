c3a42e00df65d0c872cb56852ae472671e5d69b8
Wahyu Pranata <wahyupranata131@gmail.com>
2022-12-11 22:16:56 +0800

    delete comment in useStorageAvailable

--- src/composables/useStorageAvailable.js
+++ src/composables/useStorageAvailable.js
@@ -10,12 +10,4 @@ export function useStorageAvailable(type) {
     catch(error) {
         return error
     }
-        /* i Don't know why you need to call DOMException for error even thou you can't actually just return the error itself
-        if you want to use it just uncomment the following line
-        return error instanceof DOMException && (
-            error.code === 22 || error.code === 1014 || error.name === "QuotaExceededError" ||  error.name === "NS_ERROR_DOM_QUOTA_REACHED"
-        ) && (storage && storage.length !== 0);
-    }
-
-         */
 }
