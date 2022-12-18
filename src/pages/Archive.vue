9f807019df752fa96e2e8dd6c9ebe3613a91e828
Wahyu Pranata <wahyupranata131@gmail.com>
2022-12-18 13:39:17 +0800

    add delete in archive page

--- src/pages/Archive.vue
+++ src/pages/Archive.vue
@@ -1,12 +1,33 @@
 <script setup>
-import Layout from './layout/Layout.vue';
-
 import { ref } from 'vue';
+import { createConfirmDialog } from 'vuejs-confirm-dialog'
+
+import Layout from './layout/Layout.vue';
 import ToDo from '../components/ToDo.vue';
+import ModalDialog from '../components/ModalDialog.vue'
 
 import { useGetToDos } from "../composables/useGetToDos.js";
 
+const { reveal, onConfirm } = createConfirmDialog(ModalDialog)
+
 let toDos = ref(useGetToDos("archived").data);
+function deleteToDo(id) {
+    reveal()
+    onConfirm(() => {
+        let existingData = useGetToDos();
+        let parsedToDos = existingData.data.map((e) => JSON.parse(e));
+        let stringToDos = [];
+        for (let i = 0; i < parsedToDos.length; i++) {
+            if (parsedToDos[i].id == id) {
+                parsedToDos.splice(i, 1);
+            }
+        }
+        parsedToDos.forEach(e => stringToDos.push(JSON.stringify(e)));
+        existingData.data = stringToDos;
+        localStorage.setItem("toDos", JSON.stringify(existingData));
+        toDos.value = useGetToDos().data;
+    })
+}
 </script>
 
 <template>
@@ -17,7 +38,7 @@ let toDos = ref(useGetToDos("archived").data);
                     :description="JSON.parse(toDo).description" :to-do-id="JSON.parse(toDo).id">
                     <template #list>
                         <ul>
-                            <li @click="restore(JSON.parse(toDo))">Delete</li>
+                            <li @click="deleteToDo(JSON.parse(toDo).id)">Delete</li>
                         </ul>
                     </template>
                 </ToDo>
